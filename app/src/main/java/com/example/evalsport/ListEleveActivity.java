6a116acb63846a5caf12269e67dfb66ac4af9ed8
Alexis <ifalexiscan@gmail.com>
2022-01-13 23:41:35 +0100

    update gg2

--- app/src/main/java/com/example/evalsport/ListEleveActivity.java
+++ app/src/main/java/com/example/evalsport/ListEleveActivity.java
@@ -70,14 +70,7 @@ public class ListEleveActivity extends AppCompatActivity implements ElevesRecycl
                 } else {
                     complete = "I";
                 }
-                for (int j = 0; j < e.getJSONArray("notes").length(); j++) {
-                    note += e.getJSONArray("notes").getJSONObject(j).getDouble("note");
-                }
-                if (note == (int) note) {
-                    noteString = String.format("%d",(int)note);
-                } else {
-                    noteString = String.format("%s",note);
-                }
+                noteString = getNoteFromEleve(e);
 
                 listeEleves.add(e.getString("prenomEleve") + " " + e.getString("nomEleve") + " - " + noteString + "/20" + " - " + complete);
             } catch (JSONException jsonException) {
@@ -98,7 +91,7 @@ public class ListEleveActivity extends AppCompatActivity implements ElevesRecycl
         chronoButton.setOnClickListener(view -> {
             Intent chronoActivity = new Intent(this, ChronoActivity.class);
             String[] elevesSelectiones = new String[selectionned.size()];
-            for (int i= 0; i < selectionned.size();i++){
+            for (int i= 0; i < selectionned.size(); i++){
                 elevesSelectiones[i] = cleanName(listeEleves.get(Integer.parseInt(selectionned.get(i))));
             }
 
@@ -142,6 +135,24 @@ public class ListEleveActivity extends AppCompatActivity implements ElevesRecycl
         nbSelectionnedTextView.setText(selectionned.size() + " / 4 élèves selectionnés" );
         checkButtons();
 
+    }
+
+    public static String getNoteFromEleve(JSONObject eleve){
+        double note =0;
+        String result = "0";
+        try {
+            for (int j = 0; j < eleve.getJSONArray("notes").length(); j++) {
+                note += eleve.getJSONArray("notes").getJSONObject(j).getDouble("note");
+            }
+        } catch (JSONException e) {
+                e.printStackTrace();
+        }
+        if (note == (int) note) {
+            result = String.format("%d",(int)note);
+        } else {
+            result = String.format("%s",note);
+        }
+        return result;
     }
 
     public void checkButtons() {
